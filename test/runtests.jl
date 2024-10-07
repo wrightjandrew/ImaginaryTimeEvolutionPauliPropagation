e8c25f0a34288ccafaa09ee5d42293db1a727cfc
MSRudolph <manuel.rudolph@web.de>
2024-10-07 11:41:37 +0200

    adding unit tests from example notebook

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,10 @@ using Test
 
 @testset "PauliPropagation.jl" begin
     # Write your tests here.
+    include("test_mergingbfs.jl")
+    numericalPP(8, 4, Inf, 0.0) ≈ 0.217200
+    hybridPP(8, 4, Inf, 0.0, Inf) ≈ 0.217200
+    surrogatePP(8, 4, Inf, Inf) ≈ 0.217200
 end
+
+
---
+++ test/test_mergingbfs.jl
@@ -0,0 +1,77 @@
+using Random
+
+
+function numericalPP(nq, nl, W, min_abs_coeff)
+
+    symbs = [:I for _ in 1:nq]
+    symbs[round(Int, nq / 2)] = :Z   # as symbol. Also works but is slower.
+
+    obsint = symboltoint(symbs)  # for performance we work with bitoperations
+    show(obsint, nq)
+
+    topo = bricklayertopology(nq; periodic=false)
+    # topo = get2dtopology(4, 4)
+    circ = hardwareefficientcircuit(nq, nl; topology=topo)
+    fastcirc = tofastgates(circ)
+    m = length(fastcirc)
+
+    Random.seed!(42)
+    thetas = randn(m)
+
+    dnum = mergingbfs(fastcirc, obsint, thetas; max_weight=W, min_abs_coeff=min_abs_coeff)
+    show(dnum)
+    return evalagainstzero(dnum) # expectation
+end
+
+
+
+function hybridPP(nq, nl, W, min_abs_coeff, max_freq)
+
+    symbs = [:I for _ in 1:nq]
+    symbs[round(Int, nq / 2)] = :Z   # as symbol. Also works but is slower.
+
+    obsint = symboltoint(symbs)  # for performance we work with bitoperations
+    show(obsint, nq)
+
+    topo = bricklayertopology(nq; periodic=false)
+    # topo = get2dtopology(4, 4)
+    circ = hardwareefficientcircuit(nq, nl; topology=topo)
+    fastcirc = tofastgates(circ)
+    m = length(fastcirc)
+
+    Random.seed!(42)
+    thetas = randn(m)
+
+    dhyb = mergingbfs(fastcirc, obsint, NumericPathProperties(1.0), thetas; max_weight=W, max_freq=max_freq, min_abs_coeff=min_abs_coeff)
+    show(dhyb)
+    return evalagainstzero(dhyb)
+end
+
+
+function surrogatePP(nq, nl, W, max_freq)
+
+    symbs = [:I for _ in 1:nq]
+    symbs[round(Int, nq / 2)] = :Z   # as symbol. Also works but is slower.
+
+    obsint = symboltoint(symbs)  # for performance we work with bitoperations
+    show(obsint, nq)
+
+    topo = bricklayertopology(nq; periodic=false)
+    # topo = get2dtopology(4, 4)
+    circ = hardwareefficientcircuit(nq, nl; topology=topo)
+    fastcirc = tofastgates(circ)
+    m = length(fastcirc)
+
+    Random.seed!(42)
+    thetas = randn(m)
+
+    dsym = mergingbfs(circ, operatortopathdict(obsint), zeros(m); max_weight=W, max_freq=max_freq)
+
+    final_nodes = collect(pth.coeff for (obs, pth) in zerofilter(dsym))
+    final_eval_node = PauliGateNode(parents=final_nodes, trig_inds=zeros(Int, length(final_nodes)), signs=ones(length(final_nodes)), param_idx=1, cummulative_value=0.0)
+    resetnodes(final_eval_node)
+    resetnodes(final_eval_node)
+    eval_list = gettraceevalorder(final_eval_node, zeros(m))
+
+    return expectation(eval_list, thetas)
+end
