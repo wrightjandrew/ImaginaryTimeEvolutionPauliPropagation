212e117d97a27f8681ff3814ea396e36291886e2
MSRudolph <manuel.rudolph@web.de>
2024-12-02 15:44:48 +0100

    rename Propagation/template.jl to Propagation/generics.jl

--- src/Propagation/Propagation.jl
+++ src/Propagation/Propagation.jl
@@ -1,2 +1,5 @@
-include("template.jl")
+# generics.jl contains the core functionality of the `propagation` function.
+include("generics.jl")
+
+# specializations.jl contains specialized implementations of lower level propagation functions for specific gates. 
 include("specializations.jl")
