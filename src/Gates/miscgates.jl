f0eb751e266086fc37eef485894925e5dbf8982c
MSRudolph <manuel.rudolph@web.de>
2025-01-24 15:33:01 -0500

    [bugfix] make TGate subtype StaticGate

--- src/Gates/miscgates.jl
+++ src/Gates/miscgates.jl
@@ -6,11 +6,11 @@
 
 
 """
-    TGate(qind::Integer)
+    TGate(qind::Integer) <: StaticGate
 
 Returns a T gate acting on qubit `qind`.
 It acts on qubit `qind` like a `PauliRotation(:Z, qind)` with angle π/4.
 """
-struct TGate
+struct TGate <: StaticGate
     qind::Int
 end
